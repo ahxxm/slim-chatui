feaf25d1cc0385f12294c94f26cb3e2ff1772180
ahxxm <i@ahxxm.com>
2026-03-08 00:01:52 +0900

    get i18n with type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,6 @@
+import type { i18n } from 'i18next';
+import type { Writable } from 'svelte/store';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -7,6 +10,10 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+}
+
+declare module 'svelte' {
+	export function getContext(key: 'i18n'): Writable<i18n>;
 }
 
 export {};
