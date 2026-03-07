2765aa81f353fe3b28d07fb2357f9b81c0e95619
ahxxm <i@ahxxm.com>
2026-03-07 11:58:43 +0900

    bits-ui state instead of <>Svelte condition sync

--- src/lib/components/common/Switch.svelte
+++ src/lib/components/common/Switch.svelte
@@ -28,12 +28,10 @@
 		bind:checked={state}
 		{id}
 		aria-labelledby={ariaLabelledbyId}
-		class="flex h-[1.125rem] min-h-[1.125rem] w-8 shrink-0 cursor-pointer items-center rounded-full px-1 mx-[1px] transition  {($settings?.highContrastMode ??
+		class="flex h-[1.125rem] min-h-[1.125rem] w-8 shrink-0 cursor-pointer items-center rounded-full px-1 mx-[1px] transition bg-gray-200 dark:bg-transparent data-[state=checked]:bg-emerald-500 data-[state=checked]:dark:bg-emerald-700 {($settings?.highContrastMode ??
 		false)
 			? 'focus:outline focus:outline-2 focus:outline-gray-800 focus:dark:outline-gray-200'
-			: 'outline outline-1 outline-gray-100 dark:outline-gray-800'} {state
-			? ' bg-emerald-500 dark:bg-emerald-700'
-			: 'bg-gray-200 dark:bg-transparent'}"
+			: 'outline outline-1 outline-gray-100 dark:outline-gray-800'}"
 		onCheckedChange={async () => {
 			await tick();
 			dispatch('change', state);
